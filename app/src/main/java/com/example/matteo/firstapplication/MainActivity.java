bf964566ee3112fb69355634d1f089484b74a2a9
IlBeppe <matteo.faggian12@gmail.com>
2017-09-28 00:32:03 +0200

    Aggiunta seconda attività

--- app/src/main/java/com/example/matteo/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/matteo/firstapplication/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.matteo.firstapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.matteo.firstapplication.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendMessage(View view) {}
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 
 }
